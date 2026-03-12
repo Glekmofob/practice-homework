136ceb43c32696860099413dab8c9ab672c7a3a1
Shanechka <grisacernyh@gmail.com>
2026-03-13 02:58:55 +0300

    Homework for 14.03.2026

--- solutions/sem02/lesson04/task1.py
+++ solutions/sem02/lesson04/task1.py
@@ -2,16 +2,40 @@ import numpy as np
 
 
 def pad_image(image: np.ndarray, pad_size: int) -> np.ndarray:
-    # ваш код
-    return image
+    if pad_size < 1:
+        raise ValueError
+    if np.ndim(image) == 2:
+        hieght, length = image.shape
+        new_height, new_length = hieght + pad_size * 2, length + pad_size * 2
+        padded_image = np.zeros((new_height, new_length), dtype=image.dtype)
+        padded_image[pad_size : pad_size + hieght, pad_size : pad_size + length] = image
+    else:
+        hieght, length, depth = image.shape
+        new_height, new_length = hieght + pad_size * 2, length + pad_size * 2
+        padded_image = np.zeros((new_height, new_length, depth), dtype=image.dtype)
+        padded_image[pad_size : pad_size + hieght, pad_size : pad_size + length, ...] = image
+    return padded_image
 
 
 def blur_image(
     image: np.ndarray,
     kernel_size: int,
 ) -> np.ndarray:
-    # ваш код
-    return image
+    if kernel_size < 1 or kernel_size % 2 == 0:
+        raise ValueError
+    if kernel_size == 1:
+        return image
+    pad = kernel_size // 2
+    padded_image = pad_image(image, pad)
+    shifted = [
+        padded_image[i : i + image.shape[0], j : j + image.shape[1], ...]
+        for i in range(kernel_size)
+        for j in range(kernel_size)
+    ]
+    # сверху сам массив а в глубину уходят элементы вокруг и потом мы просто ищем среднее вглубь
+    blurred = np.mean(shifted, axis=0)
+
+    return blurred.astype(image.dtype)
 
 
 if __name__ == "__main__":
--- solutions/sem02/lesson04/task2.py
+++ solutions/sem02/lesson04/task2.py
@@ -5,6 +5,26 @@ def get_dominant_color_info(
     image: np.ndarray[np.uint8],
     threshold: int = 5,
 ) -> tuple[np.uint8, float]:
-    # ваш код
+    if threshold < 1:
+        raise ValueError("threshold must be positive")
 
-    return 0, 0
+    pixels = image.flatten()
+    counts = np.zeros(256, dtype=np.int64)
+    for color in range(256):
+        mask = pixels == color
+        counts[color] = np.sum(mask)
+
+    max_pixels = 0
+    best_color = 0
+    for current_color in range(256):
+        if counts[current_color] != 0:
+            low = max(0, current_color - (threshold - 1))
+            high = min(255, current_color + (threshold - 1))
+            current_sum = np.sum(counts[low : high + 1])
+
+            if current_sum > max_pixels:
+                max_pixels = current_sum
+                best_color = current_color
+
+    percentage = (max_pixels / pixels.size) * 100
+    return np.uint8(best_color), float(percentage)
