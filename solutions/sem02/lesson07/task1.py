52c37f5b43fca53e19744e3107dbbb0acc475065
Shanechka <grisacernyh@gmail.com>
2026-04-10 04:12:00 +0300

    Hw for 11.04.2026

--- solutions/sem02/lesson07/task1.py
+++ solutions/sem02/lesson07/task1.py
@@ -13,8 +13,56 @@ def visualize_diagrams(
     ordinates: np.ndarray,
     diagram_type: Any,
 ) -> None:
-    # ваш код
-    pass
+    if abscissa.shape[0] != ordinates.shape[0] or diagram_type not in ["hist", "box", "violin"]:
+        raise ShapeMismatchError
+    figure = plt.figure(figsize=(8, 8))
+    grid = plt.GridSpec(4, 4, wspace=0.2, hspace=0.2)
+    scat_graph = figure.add_subplot(grid[0:-1, 1:])
+    ord_vert_graph = figure.add_subplot(grid[0:-1, 0], sharey=scat_graph)
+    abs_hor_graph = figure.add_subplot(grid[-1, 1:], sharex=scat_graph)
+    scat_graph.scatter(abscissa, ordinates, color="red", alpha=0.5)
+    if diagram_type == "box":
+        ord_vert_graph.boxplot(
+            ordinates,
+            patch_artist=True,
+            boxprops={"facecolor": "red", "linewidth": 2},
+            medianprops={"color": "black"},
+        )
+        abs_hor_graph.boxplot(
+            abscissa,
+            vert=False,
+            patch_artist=True,
+            boxprops={"facecolor": "red", "linewidth": 2},
+            medianprops={"color": "black"},
+        )
+    if diagram_type == "hist":
+        ord_vert_graph.hist(
+            ordinates, orientation="horizontal", bins=50, color="red", alpha=0.5, edgecolor="black"
+        )
+        abs_hor_graph.hist(abscissa, bins=50, color="red", alpha=0.5, edgecolor="black")
+        ord_vert_graph.invert_xaxis()
+        abs_hor_graph.invert_yaxis()
+
+    if diagram_type == "violin":
+        viol_vert = ord_vert_graph.violinplot(
+            ordinates,
+            showmedians=True,
+        )
+        for body in viol_vert["bodies"]:
+            body.set_facecolor("red")
+            body.set_edgecolor("black")
+        for parts in viol_vert:
+            if parts == "bodies":
+                continue
+            viol_vert[parts].set_edgecolor("black")
+        viol_hor = abs_hor_graph.violinplot(abscissa, vert=False, showmedians=True)
+        for body in viol_hor["bodies"]:
+            body.set_facecolor("red")
+            body.set_edgecolor("black")
+        for parts in viol_hor:
+            if parts == "bodies":
+                continue
+            viol_hor[parts].set_edgecolor("black")
 
 
 if __name__ == "__main__":
--- solutions/sem02/lesson07/task2.py
+++ solutions/sem02/lesson07/task2.py
@@ -1,1 +1,44 @@
 # ваш код (используйте функции или классы для решения данной задачи)
+import json
+
+import matplotlib.pyplot as plt
+import numpy as np
+
+
+def get_counts(tier_list, stages_order):
+    arr = np.array(tier_list)
+    return np.array([np.sum(arr == stage) for stage in stages_order])
+
+
+def solution():
+    plt.style.use("ggplot")
+    with open("solutions\sem02\lesson07\data\medic_data.json") as f:
+        data = json.load(f)
+
+    stages_order = ["I", "II", "III", "IV"]
+
+    before_count = get_counts(data["before"], stages_order)
+    after_count = get_counts(data["after"], stages_order)
+
+    fig, ax = plt.subplots(figsize=(12, 7))
+    x = np.arange(len(stages_order))
+    width = 0.35
+
+    ax.bar(
+        x - width / 2, before_count, width, label="До установки", color="red", edgecolor="black"
+    )
+    ax.bar(
+        x + width / 2, after_count, width, label="После установки", color="green", edgecolor="black"
+    )
+    ax.set_title("Стадии митральной недостаточности")
+    ax.set_ylabel("Кол-во людей")
+    ax.set_xticks(x)
+    ax.set_xticklabels(stages_order)
+    ax.legend()
+    fig.tight_layout()
+
+    plt.savefig("result.png")
+    plt.show()
+
+if __name__ == "__main__":
+    solution()
