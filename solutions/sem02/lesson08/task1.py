6964a4ea0815aedfefae5110bf3d272f87cf3d04
Shanechka <grisacernyh@gmail.com>
2026-04-24 21:54:23 +0300

    hell

--- solutions/sem02/lesson08/task1.py
+++ solutions/sem02/lesson08/task1.py
@@ -7,6 +7,61 @@ from IPython.display import HTML
 from matplotlib.animation import FuncAnimation
 
 
+        
+    
+    
+
+
+
+class SignalAnimate():
+    def __init__(self, fc, modulation , num_frames, plot_duration, time_step, animation_step, path):
+        self.num_frames = num_frames
+        self.plot_duration = plot_duration
+        self.time_step = time_step
+        self.animation_step = animation_step
+        self.fc = fc
+        self.modulation = modulation
+        self.path = path
+
+    def calcSig(self, t: np.array):
+        signal = np.sin(2 * np.pi * self.fc * t)
+        if self.modulation is not None:
+            return signal * self.modulation(t)
+        return signal
+
+    def update(self, frame, *,  line: plt.Line2D):
+        frameStart = frame * self.animation_step
+        frameEnd = frameStart + self.plot_duration
+        t = np.arange(frameStart, frameEnd, self.time_step)
+        signal = self.calcSig(t)
+        line.set_data(t, signal)
+        line.axes.set_xlim(frameStart, frameEnd)
+        return line,
+    
+    def animate(self):
+        figure, axis = plt.subplots(figsize= (16,9))
+        line, = axis.plot([],[], color = "red")
+        axis.set_ylim(-2, 2)
+        axis.set_title("Анимация модулированного сигнала")
+        axis.set_ylabel("Амплитуда")
+        axis.set_xlabel("Время")
+        animation = FuncAnimation(
+            figure,
+            partial(self.update, line = line),
+            frames= self.num_frames,
+            interval= 20,
+
+        )
+        if self.path != "":
+            animation.save(self.path, writer = "pillow",fps = 24)
+        return animation
+
+
+
+
+
+
+
 def create_modulation_animation(
     modulation, 
     fc, 
@@ -16,8 +71,8 @@ def create_modulation_animation(
     animation_step=0.01,
     save_path=""
 ) -> FuncAnimation:
-    # ваш код
-    return FuncAnimation()
+    sig = SignalAnimate(fc, modulation, num_frames, plot_duration, time_step, animation_step, save_path)
+    return sig.animate()
 
 
 if __name__ == "__main__":
@@ -41,3 +96,4 @@ if __name__ == "__main__":
         save_path=save_path_with_modulation
     )
     HTML(animation.to_jshtml())
+    plt.show()
