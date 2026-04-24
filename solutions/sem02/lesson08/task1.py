e03d066d3b2774c9a04f33c7fbdcec26f91db4c6
Shanechka <grisacernyh@gmail.com>
2026-04-24 22:41:22 +0300

    Hellv2

--- solutions/sem02/lesson08/task1.py
+++ solutions/sem02/lesson08/task1.py
@@ -2,19 +2,12 @@ from functools import partial
 
 import matplotlib.pyplot as plt
 import numpy as np
-
 from IPython.display import HTML
 from matplotlib.animation import FuncAnimation
 
 
-        
-    
-    
-
-
-
-class SignalAnimate():
-    def __init__(self, fc, modulation , num_frames, plot_duration, time_step, animation_step, path):
+class SignalAnimate:
+    def __init__(self, fc, modulation, num_frames, plot_duration, time_step, animation_step, path):
         self.num_frames = num_frames
         self.plot_duration = plot_duration
         self.time_step = time_step
@@ -29,61 +22,52 @@ class SignalAnimate():
             return signal * self.modulation(t)
         return signal
 
-    def update(self, frame, *,  line: plt.Line2D):
+    def update(self, frame, *, line: plt.Line2D):
         frameStart = frame * self.animation_step
         frameEnd = frameStart + self.plot_duration
         t = np.arange(frameStart, frameEnd, self.time_step)
         signal = self.calcSig(t)
         line.set_data(t, signal)
         line.axes.set_xlim(frameStart, frameEnd)
-        return line,
-    
+        return (line,)
+
     def animate(self):
-        figure, axis = plt.subplots(figsize= (16,9))
-        line, = axis.plot([],[], color = "red")
+        figure, axis = plt.subplots(figsize=(16, 9))
+        (line,) = axis.plot([], [], color="red")
         axis.set_ylim(-2, 2)
         axis.set_title("Анимация модулированного сигнала")
         axis.set_ylabel("Амплитуда")
         axis.set_xlabel("Время")
         animation = FuncAnimation(
             figure,
-            partial(self.update, line = line),
-            frames= self.num_frames,
-            interval= 20,
-
+            partial(self.update, line=line),
+            frames=self.num_frames,
+            interval=20,
         )
         if self.path != "":
-            animation.save(self.path, writer = "pillow",fps = 24)
+            animation.save(self.path, writer="pillow", fps=24)
         return animation
 
 
-
-
-
-
-
 def create_modulation_animation(
-    modulation, 
-    fc, 
-    num_frames, 
-    plot_duration, 
-    time_step=0.001, 
-    animation_step=0.01,
-    save_path=""
+    modulation, fc, num_frames, plot_duration, time_step=0.001, animation_step=0.01, save_path=""
 ) -> FuncAnimation:
-    sig = SignalAnimate(fc, modulation, num_frames, plot_duration, time_step, animation_step, save_path)
+    sig = SignalAnimate(
+        fc, modulation, num_frames, plot_duration, time_step, animation_step, save_path
+    )
     return sig.animate()
 
 
 if __name__ == "__main__":
-    def modulation_function(t):
-        return np.cos(t * 6) 
 
-    num_frames = 100  
-    plot_duration = np.pi / 2 
-    time_step = 0.001  
-    animation_step = np.pi / 200 
-    fc = 50  
+    def modulation_function(t):
+        return np.cos(t * 6)
+
+    num_frames = 100
+    plot_duration = np.pi / 2
+    time_step = 0.001
+    animation_step = np.pi / 200
+    fc = 50
     save_path_with_modulation = "modulated_signal.gif"
 
     animation = create_modulation_animation(
@@ -93,7 +77,7 @@ if __name__ == "__main__":
         plot_duration=plot_duration,
         time_step=time_step,
         animation_step=animation_step,
-        save_path=save_path_with_modulation
+        save_path=save_path_with_modulation,
     )
     HTML(animation.to_jshtml())
     plt.show()
--- solutions/sem02/lesson08/task2.py
+++ solutions/sem02/lesson08/task2.py
@@ -1,37 +1,35 @@
 from functools import partial
 from queue import Queue
+
 import matplotlib.pyplot as plt
 import numpy as np
-
-from IPython.display import HTML
 from matplotlib.animation import FuncAnimation
 
-class DrawMaze():
-    
+
+class DrawMaze:
     def __init__(self, maze, start, finish, path):
         self.maze = maze
         self.start = start
         self.finish = finish
         self.save_path = path
-        self._path  = []
+        self._path = []
         self._colors = []
         self._shortest_path = []
         self._reachEndFlag = False
         self._msgCounter = 0
         self._possible_dir = [(-1, 0), (0, -1), (1, 0), (0, 1)]
+
     def _draw(self):
-        figure, axis = plt.subplots(figsize=(16,9))
-        axis.imshow(self.maze, cmap= "binary_r")
+        figure, axis = plt.subplots(figsize=(16, 9))
+        axis.imshow(self.maze, cmap="binary_r")
         return figure, axis
+
     def _findsolution(self):
         self._path = [list(self.start)]
-        maze_wave = np.where(self.maze != 0, -1 , -2)
+        maze_wave = np.where(self.maze != 0, -1, -2)
         maze_wave[self.start[0], self.start[1]] = 0
         queue_frontier = Queue()
         queue_frontier.put(self.start)
-
-
-
 
         while not queue_frontier.empty():
             cur_elem = queue_frontier.get()
@@ -42,16 +40,17 @@ class DrawMaze():
                 temp = [cur_elem[0] + dx, cur_elem[1] + dy]
                 if 0 <= temp[0] < maze_wave.shape[0] and 0 <= temp[1] < maze_wave.shape[1]:
                     if maze_wave[temp[0], temp[1]] == -1:
-                        maze_wave[temp[0],temp[1]] = cur_dist + 1
+                        maze_wave[temp[0], temp[1]] = cur_dist + 1
                         self._path.append(temp)
                         queue_frontier.put(temp)
 
         self.maze_solution = maze_wave
         return
+
     def _backtrack(self):
         if self.maze_solution[self.finish[0], self.finish[1]] <= 0 and self.start != self.finish:
             print("There is no end")
-            return 
+            return
         cur = self.finish
         self._shortest_path.append(cur)
         print(self.start)
@@ -59,7 +58,10 @@ class DrawMaze():
             cur_dist = self.maze_solution[cur[0], cur[1]]
             for dx, dy in self._possible_dir:
                 temp = [cur[0] + dx, cur[1] + dy]
-                if 0 <= temp[0] < self.maze_solution.shape[0] and 0 <= temp[1] < self.maze_solution.shape[1]:
+                if (
+                    0 <= temp[0] < self.maze_solution.shape[0]
+                    and 0 <= temp[1] < self.maze_solution.shape[1]
+                ):
                     if self.maze_solution[temp[0], temp[1]] == cur_dist - 1:
                         cur = temp
                         self._shortest_path.append(cur)
@@ -67,7 +69,7 @@ class DrawMaze():
 
                         break
         return
-        
+
     def update(self, frame, scate, axes, nums, visited):
         if frame == 0:
             visited.clear()
@@ -79,21 +81,23 @@ class DrawMaze():
             x, y = self._path[frame]
             search_msg = ["Searching.", "Searching..", "Searching..."]
             visited.append([y, x])
-            if ((x, y) == self.finish):
+            if (x, y) == self.finish:
                 self._reachEndFlag = True
-                plt.title("This maze has a solution", loc="center", c= "green")
+                plt.title("This maze has a solution", loc="center", c="green")
                 self._colors.append("green")
-            
+
             else:
                 if frame == len(self._path) - 1 and not self._reachEndFlag:
-                    plt.title("No solution found", loc = "center", c= "red")
+                    plt.title("No solution found", loc="center", c="red")
                 elif not self._reachEndFlag:
-                    plt.title(search_msg[self._msgCounter % 3], loc= "center", c= "blue")
+                    plt.title(search_msg[self._msgCounter % 3], loc="center", c="blue")
                     self._msgCounter += 1
                 self._colors.append("blue")
 
             if self.maze.size < 400:
-                txt = axes.text(y, x, self.maze_solution[x, y], color="red", ha= "center", va= "center")
+                txt = axes.text(
+                    y, x, self.maze_solution[x, y], color="red", ha="center", va="center"
+                )
                 nums.append(txt)
         else:
             backtrack_frame = frame - len(self._path)
@@ -103,7 +107,7 @@ class DrawMaze():
                 if target in visited:
                     idx = visited.index(target)
                     self._colors[idx] = "lime"
-                
+
                 if backtrack_frame == len(self._shortest_path) - 1:
                     plt.title("Shortest path highlighted!", loc="center", c="lime")
 
@@ -111,48 +115,51 @@ class DrawMaze():
         scate.set_facecolors(self._colors)
         return [scate]
 
-    
-
-
     def animate(self):
-        
         self._findsolution()
         print(self.maze_solution)
         self._backtrack()
         print("Backtrack counted")
         figure, axis = self._draw()
         print(len(self._path))
-        scat = axis.scatter([],[], s = 100)
-        nums =[]
+        scat = axis.scatter([], [], s=100)
+        nums = []
         visited = []
         total_frames = len(self._path) + len(self._shortest_path)
         print(self._shortest_path)
-        animation = FuncAnimation(figure, partial(self.update, scate = scat, axes = axis, nums = nums, visited = visited), frames= range(total_frames), blit= True, interval= 300, repeat= True)
+        animation = FuncAnimation(
+            figure,
+            partial(self.update, scate=scat, axes=axis, nums=nums, visited=visited),
+            frames=range(total_frames),
+            blit=True,
+            interval=300,
+            repeat=True,
+        )
         if self.save_path != "":
-            animation.save(self.save_path, writer = "pillow",fps = 30)
+            animation.save(self.save_path, writer="pillow", fps=30)
         return animation
 
 
 def animate_wave_algorithm(
-    maze: np.ndarray, 
-    start: tuple[int, int], 
-    end: tuple[int, int], 
-    save_path: str = ""
-)  -> FuncAnimation:
+    maze: np.ndarray, start: tuple[int, int], end: tuple[int, int], save_path: str = ""
+) -> FuncAnimation:
     maze = DrawMaze(maze, start, end, save_path)
     return maze.animate()
 
+
 if __name__ == "__main__":
     # Пример 1
-    maze = np.array([
-        [0, 0, 0, 0, 0, 0, 0],
-        [0, 1, 1, 1, 1, 1, 0],
-        [1, 1, 0, 1, 0, 1, 0],
-        [0, 0, 1, 1, 0, 1, 0],
-        [0, 0, 0, 0, 0, 1, 0],
-        [1, 1, 1, 1, 1, 1, 0],
-        [0, 0, 0, 0, 0, 0, 0],
-    ])
+    maze = np.array(
+        [
+            [0, 0, 0, 0, 0, 0, 0],
+            [0, 1, 1, 1, 1, 1, 0],
+            [1, 1, 0, 1, 0, 1, 0],
+            [0, 0, 1, 1, 0, 1, 0],
+            [0, 0, 0, 0, 0, 1, 0],
+            [1, 1, 1, 1, 1, 1, 0],
+            [0, 0, 0, 0, 0, 0, 0],
+        ]
+    )
 
     start = (2, 0)
     end = (5, 0)
@@ -160,10 +167,11 @@ if __name__ == "__main__":
 
     animation = animate_wave_algorithm(maze, start, end, save_path)
     # HTML(animation.to_jshtml())
-    
+
     # Пример 2
-    
-    maze_path = "C:\\MIPT_Progamming_studies\\Python_course\\practice-homework\\solutions\\sem02\\lesson08\\data\\maze.npy"
+
+    maze_path = ("C:\\MIPT_Progamming_studies\\Python_course\
+                 \practice-homework\\solutions\\sem02\\lesson08\\data\\maze.npy")
     loaded_maze = np.load(maze_path).astype(np.int32)
 
     # можете поменять, если захотите запустить из других точек
@@ -176,6 +184,5 @@ if __name__ == "__main__":
     print(loaded_maze)
     loaded_animation = animate_wave_algorithm(loaded_maze, start, end, loaded_save_path)
     # HTML(loaded_animation.to_jshtml())
-    
-    
+
     plt.show()
